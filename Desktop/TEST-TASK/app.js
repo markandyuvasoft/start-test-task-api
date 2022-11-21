48602768125455184e0615e7a5dd12cb23a24e6a
markandyuvasoft <markandyuvasoft408@gmail.com>
2022-11-21 11:58:55 -0800

    photo condition remove

---
+++ Desktop/TEST-TASK/app.js
@@ -0,0 +1,39 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import bodyParser from "body-parser";
+import cors from "cors";
+import authrouter from './routes/auth.js'
+
+dotenv.config()
+
+
+const app=express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+// midleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/uploads',express.static('uploads'))
+
+app.use(cors())
+
+// routes
+app.use("/",authrouter)
+
+
+const PORT=process.env.PORT||3000
+// connect mongo db atlas
+mongoose.connect(process.env.MONGO_URL,{usenewurlparser:true,}).then(()=>{
+    console.log("connected to mongodb atlas")
+}).catch(error=>{
+console.log("something wrong")
+})
+
+// server port
+app.listen(PORT,()=>{
+    console.log("server started at port http://localhost:3000");
+})
